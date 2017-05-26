0954c815fb4fee6dfc70399ef0a92b396cbfefb6
wangGFF <1615823260@qq.com>
2017-05-26 21:07:30 +0800

    .
    .

--- js/header_footer.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-	$('#wenxin').on('mouseenter',function () {
-		$('#weixin_scan').css("width","150px");
-		$('#weixin_scan').slideDown();
-	}).on('mouseleave',function () {
-		$('#weixin_scan').slideUp();
-	});
-});
