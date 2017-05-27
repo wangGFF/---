4308c4f98d5018859befd694c411abf61d9e3cfc
niufengjun <1287774147@qq.com>
2017-05-27 18:46:57 +0800

    所有文件轮播部分
    HTML文件、css文件、JS文件

--- 北京网络职业学院/js/firstPage_more.js
+++ 北京网络职业学院/js/firstPage_more.js
@@ -42,104 +42,55 @@ window._bd_share_config={
         if(allComment.children.length>=5){
         	allComment.style.borderBottom="0";
         }
-        if(allComment.children.length%5!=0){
-            var p_div=document.createElement("div") 
-            p_div.id="page";   
-        }
-        pageUp.onclick=function(){
-        	if(page.innerHTML==""){
+        // if(allComment.children.length%5!=0){
+        //     var p_div=document.createElement("div") 
+        //     p_div.id="page";   
+        // }
+        // pageUp.onclick=function(){
+        // 	if(page.innerHTML==""){
         		
-        	}
-        }
+        // 	}
+        // }
 
 	}
-
-
-
-
-
 	window.onload=function(){
-    (function(){
-        var lunbotuRight=document.getElementById("lunbotuRight");
-        var lunbotuRightUl=lunbotuRight.children[0];
-        var lunbotuRightUlLi=lunbotuRightUl.children;
-        lunbotuRightUlLi[0].style.zIndex="99";
-        lunbotuRightUlLi[0].style.opacity="1";
-        var s=0;
-        var timer;
-        function timerInter(){
-            if (s>4) {
-                s=0;
-            };
-            for (var b = 0; b < lunbotuRightUlLi.length; b++) {
-                lunbotuRightUlLi[b].style.zIndex="0";
-                lunbotuRightUlLi[b].style.opacity="0";
-                lunbotuRightUlLi[b].className="";
-            };
-            lunbotuRightUlLi[s].className="lunbotuRightChange";
-            s++;
-        }
-        timer=setInterval(timerInter, 4000)
-        var lunbotuRightPoint=document.getElementById('lunbotuRightPoint');
-        var lunbotuPointSpan=lunbotuRightPoint.children;
-        var timerA=null;
-        for (var i = 0; i < lunbotuPointSpan.length; i++) {
-            lunbotuPointSpan[i].index=i;
-            lunbotuPointSpan[i].onclick=function(){
-                var z=this.index;
-                clearInterval(timer);
-                clearInterval(timerA);
-                for (var x = 0; x < lunbotuRightUlLi.length; x++) {
-                    lunbotuRightUlLi[x].className="";
-                    lunbotuRightUlLi[x].style.zIndex="0";
-                    lunbotuRightUlLi[x].style.opacity="0";
-                };
-                lunbotuRightUlLi[z].style.zIndex="99";
-                lunbotuRightUlLi[z].style.opacity="1";
-                var s=z;
-                timerA=setInterval(timerInter,4000);
-            }
-        };
-    })();
-
-            //获取轮播小点点
-    var dian = document.getElementsByClassName('cycle-pager')[0].children;
-    //获取五张图片
-    var tu = document.getElementsByClassName('cycle-slide');
-    //轮播定时器
-    var i = 0;
-    function time_Interval(){
-        for(var j=0;j<dian.length;j++){
-            dian[j].className = "";
-            tu[j].style.opacity = '0';
-            tu[j].style.zIndex = '0';
-        }
-        if(i==4){
-            i = 0;
-        }else{
-            i++;
-        }
-        dian[i].className = "cycle-pager-active";
-        tu[i].style.opacity = "1";
-        tu[i].style.zIndex = '100';
-    }
-    var time = setInterval(time_Interval,4000);
-    //点击小点点换图
-    for(var j=0;j<dian.length;j++){
-        dian[j].index = j;
-        dian[j].onclick = function(){
-            clearInterval(time);
-            for(var j=0;j<dian.length;j++){
-                dian[j].className = "";
-                tu[j].style.opacity = '0';
-                tu[j].style.zIndex = '0';
-            }
-            this.className = "cycle-pager-active";
-            tu[this.index].style.opacity = '1';
-            tu[this.index].style.zIndex = '100';
-            i=this.index;
-            time = setInterval(time_Interval,4000);
-        }
-    }
-    
-}
+			//获取轮播小点点
+	var dian = document.getElementsByClassName('cycle-pager')[0].children;
+	//获取五张图片
+	var tu = document.getElementsByClassName('cycle-slide');
+	//轮播定时器
+	var i = 0;
+	function time_Interval(){
+		for(var j=0;j<dian.length;j++){
+			dian[j].className = "";
+			tu[j].style.opacity = '0';
+			tu[j].style.zIndex = '0';
+		}
+		if(i==4){
+			i = 0;
+		}else{
+			i++;
+		}
+		dian[i].className = "cycle-pager-active";
+		tu[i].style.opacity = "1";
+		tu[i].style.zIndex = '100';
+	}
+	var time = setInterval(time_Interval,4000);
+	//点击小点点换图
+	for(var j=0;j<dian.length;j++){
+		dian[j].index = j;
+		dian[j].onclick = function(){
+			clearInterval(time);
+			for(var j=0;j<dian.length;j++){
+				dian[j].className = "";
+				tu[j].style.opacity = '0';
+				tu[j].style.zIndex = '0';
+			}
+			this.className = "cycle-pager-active";
+			tu[this.index].style.opacity = '1';
+			tu[this.index].style.zIndex = '100';
+			i=this.index;
+			time = setInterval(time_Interval,4000);
+		}
+	}
+	}
