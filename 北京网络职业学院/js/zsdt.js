a141497ec6248a7668df9fa705786c26080c464f
wangGFF <1615823260@qq.com>
2017-05-27 17:02:15 +0800

    就业合作
    就业合作

--- 北京网络职业学院/js/zsdt.js
+++ 北京网络职业学院/js/zsdt.js
@@ -1,23 +1,30 @@
 window.onload = function(){
-	// var page = document.getElementById('page');
-	// var page_num = page.children[0];
-	// var one = page_num.children[1];
-	// var two = page_num.children[2];
-	// var three = page_num.children[3];
-	// var four = page_num.children[4];
- //    var prev = page_num.children[0];
- //    var next = page_num.children[5];
-	var Two_page = document.getElementById('Two_page');
-	var One_page = document.getElementById('One_page');
-    var Three_page = document.getElementById('Three_page');
-    var Four_page = document.getElementById('Four_page');
-
+		var Two_page = document.getElementById('Two_page');
+		var One_page = document.getElementById('One_page');
+	    var Three_page = document.getElementById('Three_page');
+	    var Four_page = document.getElementById('Four_page');
+        var num_one = document.getElementById('num_one');
+        var num_two = document.getElementById('num_two');
+        var num_three = document.getElementById('num_three');
+        var num_four = document.getElementById('num_four');
         var content_pages=document.getElementsByClassName('content_page');
         var num = document.getElementsByClassName('num');
-        // var z = 0;
+        	console.log(num)
         for (var i = 0; i < num.length; i++) {
         	num[i].index = i;
-        	// console.log(i)
+        	var next = document.getElementsByClassName('next');
+             next[0].onclick = function(){
+             	Two_page.style.display = 'block';
+                One_page.style.display = 'none';
+		        Three_page.style.display='none';
+		        Four_page.style.display='none';
+		        num_one.style.backgroundColor = 'white';
+		        num_two.style.backgroundColor = '#488FCD';
+		        num_three.style.backgroundColor = 'white';
+		        num_four.style.backgroundColor = 'white';
+		        
+                }
+            
         	num[i].onclick = function(){
         		var x=this.index;
         		console.log(x)
@@ -26,21 +33,37 @@ window.onload = function(){
 		        One_page.style.display = 'block';
 		        Three_page.style.display='none';
 		        Four_page.style.display='none';
+		        num_one.style.backgroundColor = '#488FCD';
+		        num_two.style.backgroundColor = 'white';
+		        num_three.style.backgroundColor = 'white';
+		        num_four.style.backgroundColor = 'white';
              }else if (x===1){
                 Two_page.style.display = 'block';
                 One_page.style.display = 'none';
 		        Three_page.style.display='none';
 		        Four_page.style.display='none';
+		        num_one.style.backgroundColor = 'white';
+		        num_two.style.backgroundColor = '#488FCD';
+		        num_three.style.backgroundColor = 'white';
+		        num_four.style.backgroundColor = 'white';
              }else if (x===2) {
              	Two_page.style.display = 'none';
 		        One_page.style.display = 'none';
 		        Three_page.style.display='block';
 		        Four_page.style.display='none';
+		        num_one.style.backgroundColor = 'white';
+		        num_two.style.backgroundColor = 'white';
+		        num_three.style.backgroundColor = '#488FCD';
+		        num_four.style.backgroundColor = 'white';
              }else if (x===3) {
              	Two_page.style.display = 'none';
 		        One_page.style.display = 'none';
 		        Three_page.style.display='none';
 		        Four_page.style.display='block';
+		        num_one.style.backgroundColor = 'white';
+		        num_two.style.backgroundColor = 'white';
+		        num_three.style.backgroundColor = 'white';
+		        num_four.style.backgroundColor = '#488FCD';
              }
              var prev = document.getElementsByClassName('prev');
              var next = document.getElementsByClassName('next');
@@ -55,21 +78,37 @@ window.onload = function(){
 		        One_page.style.display = 'block';
 		        Three_page.style.display='none';
 		        Four_page.style.display='none';
+		        num_one.style.backgroundColor = '#488FCD';
+		        num_two.style.backgroundColor = 'white';
+		        num_three.style.backgroundColor = 'white';
+		        num_four.style.backgroundColor = 'white';
                 }else if (x===1){
                 Two_page.style.display = 'block';
                 One_page.style.display = 'none';
 		        Three_page.style.display='none';
 		        Four_page.style.display='none';
+		        num_one.style.backgroundColor = 'white';
+		        num_two.style.backgroundColor = '#488FCD';
+		        num_three.style.backgroundColor = 'white';
+		        num_four.style.backgroundColor = 'white';
                 }else if (x===2) {
              	Two_page.style.display = 'none';
 		        One_page.style.display = 'none';
 		        Three_page.style.display='block';
 		        Four_page.style.display='none';
+		        num_one.style.backgroundColor = 'white';
+		        num_two.style.backgroundColor = 'white';
+		        num_three.style.backgroundColor = '#488FCD';
+		        num_four.style.backgroundColor = 'white';
                 }else if (x===3) {
              	Two_page.style.display = 'none';
 		        One_page.style.display = 'none';
 		        Three_page.style.display='none';
 		        Four_page.style.display='block';
+		        num_one.style.backgroundColor = 'white';
+		        num_two.style.backgroundColor = 'white';
+		        num_three.style.backgroundColor = 'white';
+		        num_four.style.backgroundColor = '#488FCD';
                 }
              }
              prev[0].onclick = function(){
@@ -82,26 +121,41 @@ window.onload = function(){
 		        One_page.style.display = 'block';
 		        Three_page.style.display='none';
 		        Four_page.style.display='none';
+		        num_one.style.backgroundColor = '#488FCD';
+		        num_two.style.backgroundColor = 'white';
+		        num_three.style.backgroundColor = 'white';
+		        num_four.style.backgroundColor = 'white';
                 }else if (x===1){
                 Two_page.style.display = 'block';
                 One_page.style.display = 'none';
 		        Three_page.style.display='none';
 		        Four_page.style.display='none';
+		        num_one.style.backgroundColor = 'white';
+		        num_two.style.backgroundColor = '#488FCD';
+		        num_three.style.backgroundColor = 'white';
+		        num_four.style.backgroundColor = 'white';
                 }else if (x===2) {
              	Two_page.style.display = 'none';
 		        One_page.style.display = 'none';
 		        Three_page.style.display='block';
 		        Four_page.style.display='none';
+		        num_one.style.backgroundColor = 'white';
+		        num_two.style.backgroundColor = 'white';
+		        num_three.style.backgroundColor = '#488FCD';
+		        num_four.style.backgroundColor = 'white';
                 }else if (x===3) {
              	Two_page.style.display = 'none';
 		        One_page.style.display = 'none';
 		        Three_page.style.display='none';
 		        Four_page.style.display='block';
+		        num_one.style.backgroundColor = 'white';
+		        num_two.style.backgroundColor = 'white';
+		        num_three.style.backgroundColor = 'white';
+		        num_four.style.backgroundColor = '#488FCD';
                 }
              	
              }
         	}
-
         }
 
 
