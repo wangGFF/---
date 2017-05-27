a8fde2f3ca1215d0e16c36f842bef4e03363f0f9
niufengjun <1287774147@qq.com>
2017-05-27 18:14:52 +0800

    轮播
    轮播下面的圆点

--- 北京网络职业学院/js/zhaoshenglunbo.js
+++ 北京网络职业学院/js/zhaoshenglunbo.js
@@ -36,6 +36,7 @@ window.onload=function(){
                 };
                 lunbotuRightUlLi[z].style.zIndex="99";
                 lunbotuRightUlLi[z].style.opacity="1";
+                lunbotuPointSpan[z].style.backgroundColor="red";
                 var s=z;
                 timerA=setInterval(timerInter,4000);
             }
